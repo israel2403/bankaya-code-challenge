5ba330146ad7ffa8f4fbe98c6d1f05bbaea67d7c
Israel Huerta <israelhf24@gmail.com>
2024-07-17 15:44:28 -0600

    First commit

---
+++ src/main/java/com/bankaya/code/challenge/pokeapi_soap_service/PokeapiSoapServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bankaya.code.challenge.pokeapi_soap_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeapiSoapServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeapiSoapServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bankaya/code/challenge/pokeapi_soap_service/PokeapiSoapServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankaya.code.challenge.pokeapi_soap_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeapiSoapServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
